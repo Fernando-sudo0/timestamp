05e13e5489ca0630ea1341b8d3df5ed00a48993b
Fernando Gutierrez <fernando.gtz.escarrega@gmail.com>
2022-06-26 14:08:45 -0700

    timestamp conversion json unix and utc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 // where your node app starts
-
+require('dotenv').config()
 // init project
 var express = require('express');
 var app = express();
@@ -15,9 +15,20 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
+  console.log(req);
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date", function (req, res) {
+  const year = parseInt(req.params.date.substring(0,4));
+  const month = parseInt(req.params.date.substring(5,7)) ;
+  const day = parseInt(req.params.date.substring(8,10));
+  const date=  new Date(year, month - 1, day,00,0,0,0);
+  const unix = Math.floor(date / 1000);
+
+  res.json({unix :  unix , utc : date.toUTCString()})
+  
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
